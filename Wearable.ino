d31e9186b28d7b35119f33ad4048fcbfc7ecb290
Craig Droke <94210673+CraigDroke@users.noreply.github.com>
2021-12-07 21:20:34 -0500

    Add files via upload

--- Wearable.ino
+++ Wearable.ino
@@ -1,43 +1,23 @@
-// The code below is for wearable
+// Wearable Device
 
-//install the below libraries
+#include <Arduino.h>
+#include <U8g2lib.h>
+ 
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
 #include <Wire.h>
-#include <Adafruit_SSD1306.h>
-#include <Adafruit_GFX.h>
+#endif
+ 
+U8G2_SSD1306_128X64_ALT0_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE); // SSD1306 and SSD1308Z are compatible
+ 
 
-//This defines the dimensions of the display
-#define OLED_WIDTH 128
-#define OLED_HEIGHT 32
-#define OLED_ADDR   0x3C
-
-Adafruit_SSD1306 display(OLED_WIDTH, OLED_HEIGHT);
-Adafruit_SI1145 uv = Adafruit_SI1145();
-
-//This sets up the display and prints headings.
-void setup() {
-  display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
-  display.clearDisplay();
-
-  display.setTextSize(.5);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 0);
-  display.println("Today's Conditions");
-
-  display.setTextSize(.5);
-  display.setTextColor(WHITE);
-  display.println("UV, Temperature and Humidity:");
-
-  display.display();
-
+void setup(void) {
+  u8g2.begin();
+ 
 }
-
-// The loop below should pull from the MQTT server and prints to the display. This should occur every 5 mins.
-void loop() {
-  
-  display.println(uv + "nm");
-  display.println(t + "°F");
-  display.println(h + "%");
-
-  delay(10);
-  display.clearDisplay();
+ void loop(){ 
+   
+// Code should be implemented here to pull measurement from server and interpret that measurement.
 }
